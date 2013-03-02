89e6489defa3de86d154639bb19d7336fa5a360b
Marie Helene Kvello-Aune <752131+Savagedlight@users.noreply.github.com>
2013-03-02 18:27:53 +0100

    Bumped ersion to 1.2.4

--- Demoder.PlanetMapViewer/Properties/AssemblyInfo.Template.cs
+++ Demoder.PlanetMapViewer/Properties/AssemblyInfo.Template.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Demoder.PlanetMapViewer")]
-[assembly: AssemblyCopyright("Copyright © Demoder (demoder@demoder.me) 2012")]
+[assembly: AssemblyCopyright("Copyright © Demoder (demoder@demoder.me) 2012, 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.3.{MERCURIAL_REVISION_NUMBER}")]
-[assembly: AssemblyFileVersion("1.2.3.{MERCURIAL_REVISION_NUMBER}")]
+[assembly: AssemblyVersion("1.2.4.{MERCURIAL_REVISION_NUMBER}")]
+[assembly: AssemblyFileVersion("1.2.4.{MERCURIAL_REVISION_NUMBER}")]
