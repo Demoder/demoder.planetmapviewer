c7ef6e95ac18661c8d6ea1d8deb50b606bfaf4fe
Marie Helene Kvello-Aune <752131+Savagedlight@users.noreply.github.com>
2013-03-02 18:29:30 +0100

    Updated license headers.

--- Lib/Demoder.AoHook/Events/CharacterLogoutEventArgs.cs
+++ Lib/Demoder.AoHook/Events/CharacterLogoutEventArgs.cs
@@ -1,6 +1,6 @@
 ﻿/*
 * Demoder.AoHook
-* Copyright (C) 2012 Demoder (demoder@demoder.me)
+* Copyright (C) 2012, 2013 Demoder (demoder@demoder.me)
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
